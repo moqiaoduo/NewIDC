08480621f20df8f7c052e962ebd6e25e21b9eef3
moqiaoduo <415372363@qq.com>
2020-02-25 22:41:06 +0800

    语言更新

--- resources/lang/zh-CN/admin.php
+++ resources/lang/zh-CN/admin.php
@@ -105,7 +105,66 @@ return [
     'quick_create'           => '快速创建',
     'help'                  => [
         'product'           => [
-            'order'         => '优先级越大排得越靠前'
+            'order'         => '优先级越大排得越靠前',
+            'free_domain'   => '前面不用加. ，用逗号或回车结束输入',
+            'require_domain'=> '用户订购时，要求用户输入有效域名',
+            'ena_stock'     => '启用后，将限制该产品的购买数量，每次下单后库存-1'
+        ],
+        'config'            => [
+            'tos'           => '启用后，用户注册/购买产品需要先同意服务条款',
+            'sug'           => '若选择“随机用户名”，则会生成sxxxxxxx样子的用户名（s加7个数字）;
+        若选择“从域名生成”，则会取域名中所有字母，若有重复则后加1',
+            'site_suu'      => '启用后，整个服务表不允许出现相同用户名的服务（启用之前不算）',
+            'site_sdu'      => '启用后，整个服务表不允许出现相同域名的服务（启用之前不算）',
+            'admin_email'   => '用于接收系统邮件(如工单提醒)',
+            'cron'          => '注意：暂停和停止时间是同时计时的，若时间一致，则优先停止服务',
+            'template'      => '点击叉号将其置空则使用默认模板',
+            'tpl_settings'  => '下面是模板设置（保存后刷新）',
+            'tpl_hp'        => '选中的产品将在首页展示，最多4个'
+        ]
+    ],
+    'config'                    => [
+        'register'              => '允许注册',
+        'tos'                   => '服务条款(TOS)',
+        'tos_url'               => '服务条款URL',
+        'sug'                   => '服务用户名生成方式',
+        'sug_domain'            => '随机用户名',
+        'sug_random'            => '从域名生成',
+        'site_suu'              => '全站服务用户名惟一',
+        'site_sdu'              => '全站服务域名惟一',
+        'admin_email'           => '管理员邮箱',
+        'cron'                  => '启用计划任务',
+        'suspend_expire'        => '暂停过期服务',
+        'suspend_after_days'    => '过期n天后暂停',
+        'terminate_expire'      => '停止过期服务',
+        'terminate_after_days'  => '过期n天后停止',
+        'template'              => '使用模板',
+        'tpl_home_product'      => '首页显示推荐产品',
+        'enable_udg'            => '启用升/降级',
+    ],
+    'product'                   => [
+        'tab'                   => [
+            'base'              => '基础',
+            'price'             => '定价',
+            'api'               => '接口设置',
+        ],
+        'ena_stock'             => '启用库存',
+        'price'                 => [
+            'addItem'           => '添加一条记录（名称留空自动删除）',
+            'name'              => '周期名称',
+            'period'            => '周期时间',
+            'price'             => '周期价格',
+            'remark'            => '备注',
+            'enable'            => '启用周期',
+            'auto_activate'     => '自动开通',
+            'allow_renew'       => '允许用本周期续费',
+        ],
+        'domain'                => [
+            'free'              => '免费域名',
+            'require'           => '要求提供域名'
+        ],
+        'server'                => [
+            'plugin'            => '服务器类型'
         ]
     ]
 ];
