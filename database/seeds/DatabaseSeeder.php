de9c2b74f4c383cf5a7544893e2a69b3a77dda28
moqiaoduo <415372363@qq.com>
2020-03-10 20:48:20 +0800

    修改Seeder名称

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class
+            AdminTablesSeeder::class
         ]);
     }
 }
