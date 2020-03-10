e5116a992febdd72b953964025518d7e9f49f650
moqiaoduo <415372363@qq.com>
2020-03-10 20:51:51 +0800

    添加中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'lang' => \App\Http\Middleware\Lang::class,
+        'admin.lang' => \App\Admin\Middleware\Lang::class,
     ];
 
     /**
