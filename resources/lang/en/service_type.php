9d296d7d46bba8b8ef7979f0fa5b7250289ad077
moqiaoduo <415372363@qq.com>
2020-03-10 20:52:56 +0800

    更新翻译

---
+++ resources/lang/en/service_type.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /**
+     * 关于一些type选项的翻译，顺便作为一个列表
+     */
+
+    'hosting'=>'Hosting',
+    'reseller'=>'Reseller',
+    'vps'=>'VPS',
+    'server'=>'Dedicated Server',
+    'others'=>'Others',
+];
