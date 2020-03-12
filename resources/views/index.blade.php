d7ab674f1acb6cd35c2f6587fd6a5fed104fedf0
moqiaoduo <415372363@qq.com>
2020-03-12 20:24:45 +0800

    首页产品推荐实装

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,32 +10,28 @@
 
 @section('content')
     <div class="layui-row">
-        <h1 style="text-align: center;">欢迎来到 {{config('app.name')}}</h1>
+        <h1 style="text-align: center;">@lang('Welcome to :site',['site'=>config('app.name')])</h1>
     </div>
     <div class="layui-row">
-        <h2 style="text-align: center;">为您推荐以下产品</h2>
+        <h2 style="text-align: center;">@lang('Recommend Products for you')</h2>
     </div>
     <div class="layui-row layui-col-space20 newidc-margin-top20">
+        @foreach(\App\Models\Product::whereIn('id',
+(array) json_decode(getOption('template_default_home_product'),true))->get() as $product)
         <div class="layui-col-md3">
             <div class="newidc-index-product">
                 <ul>
                     <li>
-                        <div>产品名</div>
-                        <div>价格</div>
+                        <div>{{$product['name']}}</div>
+                        <div>￥{{$product['price'][0]['price']??'Free'}} Start</div>
                     </li>
-                    <li>属性</li>
-                    <li>属性</li>
-                    <li>属性</li>
-                    <li>属性</li>
-                    <li>属性</li>
-                    <li>属性</li>
-                    <li>属性</li>
-                    <li>属性</li>
+                    {!! $product['description'] !!}
                     <li>
-                        <a class="layui-btn layui-btn-fluid" href="">立即购买</a>
+                        <a class="layui-btn layui-btn-fluid" href="">@lang('Buy NOW')</a>
                     </li>
                 </ul>
             </div>
         </div>
+        @endforeach
     </div>
 @endsection
