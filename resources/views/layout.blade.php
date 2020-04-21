7edb7816867f4308d1f37ee12059d5a9508e6f10
moqiaoduo <415372363@qq.com>
2020-04-22 02:04:02 +0800

    增加首页导航

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,6 +14,7 @@
     <div class="layui-header">
         <div class="layui-logo">{{config('app.name')}}</div>
         <ul class="layui-nav layui-layout-left">
+            <li class="layui-nav-item"><a href="{{url('/')}}">@lang('Home')</a></li>
             <li class="layui-nav-item"><a href="{{route('shop')}}">@lang('Store')</a></li>
         </ul>
         <ul class="layui-nav layui-layout-right">
