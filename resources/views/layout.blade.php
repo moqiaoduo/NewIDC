c69b4451842a121e81f6b579d3c77278d539075d
moqiaoduo <415372363@qq.com>
2020-06-16 00:50:13 +0800

    添加广播等功能

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - {{config('app.name')}}</title>
     <link rel="stylesheet" href="{{asset('vendor/layui/css/layui.css')}}">
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
