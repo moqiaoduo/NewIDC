fec7f052708f3812215cf919a6fa8b8a7e4aa889
moqiaoduo <415372363@qq.com>
2020-09-03 15:03:10 +0800

    fixed: 队列

--- app/Events/TicketClose.php
+++ app/Events/TicketClose.php
@@ -2,7 +2,16 @@
 
 namespace App\Events;
 
+use App\Models\Ticket;
+
 class TicketClose extends TicketEvent
 {
+    public $admin;
 
+    public function __construct(Ticket $ticket, $admin = false)
+    {
+        $this->admin = $admin;
+
+        parent::__construct($ticket);
+    }
 }
--- app/Listeners/TicketEventSubscriber.php
+++ app/Listeners/TicketEventSubscriber.php
@@ -3,15 +3,15 @@
 namespace App\Listeners;
 
 use App\Events\TicketEvent;
-use App\Models\Administrator;
 use App\Notifications\TicketAnswer;
 use App\Notifications\TicketClose;
 use App\Notifications\TicketCustomerReply;
 use App\Notifications\TicketOpen;
 use App\Notifications\TicketOpenAdmin;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Notification;
 
-class TicketEventSubscriber
+class TicketEventSubscriber implements ShouldQueue
 {
     public function handleOpen(TicketEvent $event)
     {
@@ -36,7 +36,8 @@ class TicketEventSubscriber
 
     public function handleClose(TicketEvent $event)
     {
-        $event->ticket->user->notify(new TicketClose($event->ticket));
+        if ($event->admin)
+            $event->ticket->user->notify(new TicketClose($event->ticket));
     }
 
     public function subscribe($events)
